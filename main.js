551bd7320443d98610d435b62b1ce52fa96e8e18
Carl Berthold <Carl@space-time.tv>
2021-08-27 10:09:36 +0200

    motivation the second

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ window.addEventListener("DOMContentLoaded", event => {
   audio.volume = 0.2;
   audio.play();
 });
-
+//
 //richtung 
 let direction = 'right';
 
 // geschwindigkeit entlang der x Achse +rechts -Links
-let speed = 5;  
+let speed = 5;
 
 // counter 
 document.querySelector('h2')
@@ -22,116 +22,132 @@ function preload() {
 
 
 //setup a canvas
-function setup() { 
-    // make it fullscreen (windowWidth, windowHeight)
-    createCanvas(windowWidth, windowHeight);
+function setup() {
+  // make it fullscreen (windowWidth, windowHeight)
+  createCanvas(windowWidth, windowHeight);
 
-    // position of my Snake randomly
-   //snakeX = random(50,windowWidth-50);;
-   //snakeY = random(50,windowHeight-50);
-    snakeWidth = 50;
-    snakeHeight = 50;
-    snakeX = 500;
-    snakeY = 450; 
+  // position of my Snake randomly
+  //snakeX = random(50,windowWidth-50);;
+  //snakeY = random(50,windowHeight-50);
+  snakeWidth = 50;
+  snakeHeight = 50;
+  snakeX = 500;
+  snakeY = 450;
 
 
-    
-    
-    // position of my Obstacle randomly
-   //obstacleX = random(50,windowWidth-50);
-   //obstacleY = random(50,windowHeight-50);
-    obstacleWidth = 50;
-    obstacleHeight = 50;
-    obstacleX = 500;
-    obstacleY = 500;
 
-    
-    
-       
+
+  // position of my Obstacle randomly
+  //obstacleX = random(50,windowWidth-50);
+  //obstacleY = random(50,windowHeight-50);
+  obstacleWidth = 50;
+  obstacleHeight = 50;
+  obstacleX = 500;
+  obstacleY = 500;
+
+
+
+
 }
 
-
+function placeObstacleRandomly() {
+  obstacleX = random(50, windowWidth - 50);
+  obstacleY = random(50, windowHeight - 50);
+}
 
 // check if snake have the same position as obstacle
- function colision() {
-   //if (snakeY + snakeHeight < obstacleHeight && snakeX+ snakeWidth < obstacleWidth){
-  
-if (snakeX <= obstacleX && snakeY <= obstacleY || snakeX >= obstacleX && snakeY >= obstacleY) {
-  console.log('links')
-obstacleX = random(0,950);
-obstacleY = random(0,550);
-snakeWidth
-//snakeHeight
-counter++
+function colision() {
+  // get middle of the snake 
+  const snakeMiddleX = snakeX + snakeWidth / 2;
+  const snakeMiddleY = snakeY + snakeHeight / 2;
+  // get middle of the obstacle
+  const obstacleMiddleX = obstacleX + obstacleWidth / 2;
+  const obstacleMiddleY = obstacleY + obstacleHeight / 2;
+  // check if distance between the middle of obstacle and snake
+  // is less than the width of the snake
+  if (dist(snakeMiddleX, snakeMiddleY, obstacleMiddleX, obstacleMiddleY) < snakeWidth) {
+    console.log('colission');
+    // udpate score 
+    counter++;
+    // check if score reached 10
+    if (counter === 10) {
+      alert('You made it 🙀 🥇 🚀');
+      // stops the execution of the p5 loop
+      noLoop();
+    }
+    // replace obstacle
+    placeObstacleRandomly();
+    // increment speed
+    speed += 1;
+  }
 }
 
-}
 
 
 
 // moves to the direction
-function move () {
-  if (direction === 'right'){
-  snakeX = snakeX + speed;
-  } else if (direction === 'left'){
+function move() {
+  if (direction === 'right') {
+    snakeX = snakeX + speed;
+  } else if (direction === 'left') {
     snakeX = snakeX - speed;
-  } else if (direction === 'up'){
+  } else if (direction === 'up') {
     snakeY = snakeY - speed;
-  } else if (direction === 'down'){
+  } else if (direction === 'down') {
     snakeY = snakeY + speed;
   }
 }
 
-function drawGrid(){
-  for (let i = 0; i <= windowWidth ; i += 50) {
+function drawGrid() {
+  for (let i = 0; i <= windowWidth; i += 50) {
     strokeWeight(1);
     line(0, i, windowWidth, i) // horizontal lines - y changes 
     line(i, 0, i, windowHeight) //vertical lines - x changes   
-}
+  }
 
   //line(50,0,50,600)
   //line(0,50,1000 ,50)
-  
-  
-  
- 
+
+
+
+
 }
 
 
-  
+
 // draw´s  
 function draw() {
   move();
   colision();
-//keyPressed()
-//snakeY = snakeY + speed
-// cleared Background
+  //keyPressed()
+  //snakeY = snakeY + speed
+  // cleared Background
   clear();
   stroke('green');
   fill('green');
   //draws a Obstacle
-  rect(obstacleX, obstacleY,50,50)  
+  rect(obstacleX, obstacleY, 50, 50)
   stroke('red');
   fill('red');
   //draws a snake
-  rect(snakeX,snakeY,snakeHeight,snakeWidth)
-   // Was passiert wenn ich den Bildschirmrand erreiche
-   //wenn x das rechte Ende berührt snakeX erscheint am linken Rand 
-   if (snakeX>windowWidth) {
-   snakeX = -50;
-   } else if (snakeX<=-50) {
-   snakeX = windowWidth;
-   }
-   // wenn y untere Ende berührt snakeY erscheint am oberen Rand 
-  else if (snakeY<-50){
-  snakeY = windowHeight;
-  } else if (snakeY>windowHeight){
-  snakeY = -50;
+  rect(snakeX, snakeY, snakeHeight, snakeWidth)
+  // Was passiert wenn ich den Bildschirmrand erreiche
+  //wenn x das rechte Ende berührt snakeX erscheint am linken Rand 
+  if (snakeX > windowWidth) {
+    snakeX = -50;
+  } else if (snakeX <= -50) {
+    snakeX = windowWidth;
+  }
+  // wenn y untere Ende berührt snakeY erscheint am oberen Rand 
+  else if (snakeY < -50) {
+    snakeY = windowHeight;
+  } else if (snakeY > windowHeight) {
+    snakeY = -50;
   }
   //drawGrid()
-  
-  
-}  
+
+
+}
 
 
 
@@ -140,25 +156,25 @@ function draw() {
 function keyPressed() {
   // by pressing arrow-up key rect change direction to arrow up
   // need to change direction
-  
+
   //up
-  if (keyCode === 38){
+  if (keyCode === 38) {
     direction = 'up';
-  //down
-  } else if (keyCode === 40){
+    //down
+  } else if (keyCode === 40) {
     direction = 'down';
-  //left
-  } else if (keyCode === 37){
+    //left
+  } else if (keyCode === 37) {
     direction = 'left';
-  //right
-  } else if (keyCode === 39){
+    //right
+  } else if (keyCode === 39) {
     direction = 'right';
 
     // space for stoping 
-  } else if (keyCode === 32){
+  } else if (keyCode === 32) {
     speed = 0;
     // enter for starting 
-  } else if (keyCode === 13){
+  } else if (keyCode === 13) {
     speed = 5;
   }
-} 
+}
